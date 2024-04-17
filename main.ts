2e6a8972c195fd3447ebb1ba3ba9c6610ac4c37e
timothyjeadam <92826294+timothyjeadam@users.noreply.github.com>
2024-04-17 20:13:08 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 let haloDisplay = kitronik_halo_hd.createZIPHaloDisplay(60)
-let minutes = 0
+let minutes = 13
 let seconds = 0
+let hours = 8
 haloDisplay.range(0, 1).showColor(kitronik_halo_hd.colors(ZipLedColors.Red))
 loops.everyInterval(1000, function () {
     haloDisplay.clear()
@@ -14,6 +15,10 @@ loops.everyInterval(1000, function () {
     }
     if (minutes == 60) {
         minutes = 0
+        hours += 5
+    }
+    if (hours == 60) {
+        hours = 0
     }
 })
 basic.forever(function () {
